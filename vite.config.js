2f83845f4e683475348d91a3800b385aa5b91569
engnaser2005 <engnaser2005@example.com>
2026-05-27 00:30:27 +0300

    Add base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, open: true }
+  // هذا السطر هو الذي يحل مشكلة الـ 404 على GitHub Pages
+  base: '/souqna-web/', 
+  server: { 
+    port: 3000, 
+    open: true 
+  }
 })
